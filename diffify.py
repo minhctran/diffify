1b458b9af096c7c4cf101cde0bb9e922b09fbb17
Minh Tran <congminhc1@gmail.com>
2019-05-20 16:23:33 -0700

    Formatting

--- diffify.py
+++ diffify.py
@@ -3,44 +3,51 @@ import subprocess
 from subprocess import PIPE
 import os
 import time
-######################
-def flattenFile(filein,fileout): 
+
+
+def flattenFile(filein, fileout):
     for line in filein:
         if '\input' in line:
             sub_name = line.strip()
             sub_name = sub_name[7:-1]
-            fsub = open(sub_name+'.tex','r')
-            flattenFile(fsub,fileout)
+            fsub = open(sub_name+'.tex', 'r')
+            flattenFile(fsub, fileout)
             fsub.close()
             print('Pulling from '+sub_name+'.tex')
         else:
             fileout.write(line)
+
+
 def countdown(n):
     for x in range(n):
         print(n-x)
         time.sleep(1)
-def flatten(fin_name,fout_name,commit = 'master'):
+
+
+def flatten(fin_name, fout_name, commit='master'):
     if commit != 'master':
-        myCommand(['git', 'checkout',commit])
-    fin = open(fin_name,'r') 
-    fout = open(fout_name,'w')
+        myCommand(['git', 'checkout', commit])
+    fin = open(fin_name, 'r')
+    fout = open(fout_name, 'w')
     print('Flattening '+fin_name+' into '+fout_name)
-    flattenFile(fin,fout)
+    flattenFile(fin, fout)
     fin.close()
     fout.close()
     if commit != 'master':
-        myCommand(['git', 'checkout','master'])
+        myCommand(['git', 'checkout', 'master'])
 
-# Excexute a terminal command
-def myCommand(command,stdout = None, stderr = None, shell=False):
-    print(command)    
-    output = subprocess.Popen(command, stdout=stdout, stderr=stderr,shell=shell)
+
+def myCommand(command, stdout=None, stderr=None, shell=False):
+    # Excexute a terminal command
+    print(command)
+    output = subprocess.Popen(command, stdout=stdout, stderr=stderr, shell=shell)
     stdout, stderr = output.communicate()
     print(stdout)
     print(stderr)
 
-# Delete temp files 
+
 def cleanup(fileList):
+    # Delete temp files
     for file in fileList:
         try:
             os.remove(file)
@@ -48,34 +55,36 @@ def cleanup(fileList):
         except:
             print(file+' not found')
 
-# compile TeX files into PDF
+
 def pdflatex(filename):
+    # compile TeX files into PDF
     try: 
-        myCommand('pdflatex '+filename,stdout = None, stderr = None, shell=True)
-        myCommand('bibtex '+filename[:-4],stdout = None, stderr = None, shell=True)
-        myCommand('pdflatex '+filename,stdout = None, stderr = None, shell=True)
-        myCommand('pdflatex '+filename,stdout = None, stderr = None, shell=True)
-        latexAuxList = ['.aux','.toc','.log','.syntex.gz','.bbl','.blg','Notes.bib']
+        myCommand('pdflatex '+filename, stdout=None, stderr=None, shell=True)
+        myCommand('bibtex '+filename[:-4], stdout=None, stderr=None, shell=True)
+        myCommand('pdflatex '+filename, stdout=None, stderr=None, shell=True)
+        myCommand('pdflatex '+filename, stdout=None, stderr=None, shell=True)
+        latexAuxList = ['.aux', '.toc', '.log', '.syntex.gz', '.bbl', '.blg', 'Notes.bib']
         for ext in latexAuxList:
             cleanup([filename[:-4]+ext])
     except:
         print('Something went wrong. Pdflatex failed!')
         
-# Split path into path and file name       
-def pathSplit(s):    
+
+def pathSplit(s):
+    # Split path into path and file name
     slashPos = [pos for pos, char in enumerate(s) if char == '/']
 #    print(slashPos)
     if slashPos:
         return s[0:slashPos[-1]+1],s[slashPos[-1]+1:]
     else:
-        return '',s
+        return '', s
 #    print(s[0:slashPos[-1]+1])
-#    print(s[slashPos[-1]+1:])        
-#######################            
+#    print(s[slashPos[-1]+1:])
+
+
 def diffify(mainfile, old_ver, make_pdf = True, clean_up = True):
-    
     ## move to the project folder
-    dir_name,fin_name=pathSplit(mainfile)
+    dir_name, fin_name = pathSplit(mainfile)
     try:    
         dir_path = os.path.dirname(os.path.realpath(__file__))+'/'
     except:
@@ -88,10 +97,10 @@ def diffify(mainfile, old_ver, make_pdf = True, clean_up = True):
     diffFileName = fin_name[:-4]+'-diff-'+old_ver[:6]+'.tex'
     
     ### flatten the old version
-    flatten(fin_name,flattenedOldFileName,commit = old_ver)
+    flatten(fin_name, flattenedOldFileName, commit=old_ver)
     
     ### flatten the current version
-    flatten(fin_name,flattenedNewFileName)
+    flatten(fin_name, flattenedNewFileName)
     
     ### Make a diff file
     command = 'latexdiff '+flattenedOldFileName+' '+flattenedNewFileName+' > '+diffFileName
@@ -103,11 +112,9 @@ def diffify(mainfile, old_ver, make_pdf = True, clean_up = True):
     
     ### Clean up if clean_up = True
     if clean_up:
-        cleanup([flattenedOldFileName,flattenedNewFileName,diffFileName])
+        cleanup([flattenedOldFileName, flattenedNewFileName, diffFileName])
     
     print('Finished!')
-
-
 
 
 #diffify(need_input=False,make_pdf=False,clean_up=False)

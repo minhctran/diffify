764cde43a60e317d0058ac5788dde89764f7d513
Minh Tran <congminhc1@vpn-107.umiacs.umd.edu>
2019-08-16 13:12:29 -0400

    update example

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from diffify import diffify
-
-
+import os
+# For the purpose of this example, we need the path to where the script is running
+script_path = os.path.dirname(os.path.abspath(__file__))
 # Make a diff file
 diffify(
         # Full path to the main file
-        main_path='',
+        main_path= script_path + '/example/main.tex',
         # old commit to be compared,
-        old_ver='',
+        old_ver='4a9379f25c69ce2a0879f76198f3d78239e3fd30',
         # [Optional] new commit, default value is 'master'
         new_ver='master',
         # [Optional] Make a pdf from the diff file. May throw an error if latexpdf is not properly setup

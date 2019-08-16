bf0f2021c60b489936f8df3eb18bf3bc144a96b9
Minh Tran <congminhc1@vpn-107.umiacs.umd.edu>
2019-08-16 13:00:22 -0400

    update example

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from diffify import diffify
 
 # Make a diff file
 diffify(
-        # Path to the main file
-        main_path='/Users/congminhc1/Research/papers/heating-power-law/main.tex',
+        # Full path to the main file
+        main_path='',
         # old commit to be compared,
-        old_ver='b73f425c5f48e461198d49a70f051d882d3e9eed',
+        old_ver='',
         # [Optional] new commit, default value is 'master'
         new_ver='master',
         # [Optional] Make a pdf from the diff file. May throw an error if latexpdf is not properly setup
